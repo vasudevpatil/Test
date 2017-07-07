dba5c8d8ba64bb0aa1585aa181a23b9399fc65c3
Vasudev Patil <vasudevpatil@gmail.com>
2017-07-07 19:10:54 +0530

    Initial commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var testC2DBDFF6D79F41128524788ECDF95246_DEBUG = {
+        name: 'testC2DBDFF6D79F41128524788ECDF95246_DEBUG',
+        displayName: 'Test',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.7.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.testC2DBDFF6D79F41128524788ECDF95246.Visual(options),
+        custom: true
+    };
+}
